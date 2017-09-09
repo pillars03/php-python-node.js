ba684bceda5f7c8e6333b6767b9dd772a7b69c6d
pillars <625476086@qq.com>
2017-09-09 14:38:45 +0800

    update

--- minming.php
+++ minming.php
@@ -1,20 +1,21 @@
 <?php
-$filename='xxxx.json';
+$game='king';
+
+$filename="E:/{$game}/res/samejson/army.json";
 $army=file_get_contents($filename);
 $army=json_decode($army,true);
 foreach($army as $item){
     $fileArr[]=$item['json'];
 }
 foreach($fileArr as $file){
-    $fileReName="E:/jiushen/res/".$file.'.json';
+    $fileReName="E:/{$game}/res/".$file.'.json';
     if(file_exists($fileReName)){
         $donghua=file_get_contents($fileReName);
         if((stripos($donghua,'"hit"'))===FALSE){
-                var_dump('no hit frame   filename        ->'.$file);
+            var_dump('no hit frame   filename        ->'.$file);
         }
         $donghua=json_decode($donghua,true);
         if(count($donghua['Content']['Content']['UsedResources'])>2){
-            // var_dump($donghua['Content']['Content']['UsedResources']);
             var_dump('conflict UsedResources filename->'.$file);
             continue;
         }
@@ -23,8 +24,6 @@ foreach($fileArr as $file){
         $keyName=str_replace('_','',$file);
         if($plist!='plist_'.$keyName.'.plist'){
             var_dump('illegal UsedResources filename ->'.$file);
-            echo "<br>";
         }
     }
 }
-?>
